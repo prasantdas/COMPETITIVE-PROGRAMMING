ca82cc5e59311e01a7ad2ee005b8055b61335c3e
prasantdas <93581641+prasantdas@users.noreply.github.com>
2023-11-07 02:06:51 +0530

    Add files via upload
    THIS IS A TEMPLATE OF COMPETITIVE PROGRAMMING

---
+++ 1_CP_TEMPLATE.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ * Author    : Prasant Das
+ * Date      : 15:06:2023
+ */
+#define mod 1000000007
+#define INF 1e18
+#define inf 1e18
+#define vi vector<int>
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<ll, ll> pll;
+typedef priority_queue<int> maxheap;
+typedef priority_queue<int, vector<int>, greater<int>> minheap;
+#define for0(i,n)  for(ll i=0; i<n; i++)
+#define for1(i,n)  for(ll i=1; i<=n; i++) 
+int d1[4] = { -1, 1, 0, 0};
+int d2[4] = {0, 0, -1, 1};
+int D1[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int D2[8] = { -1, 0, 1, -1, 1, -1, 0, 1 }; 
+
+void solve() {
+    //Write Here
+    cout<<"Hello World!"<<endl;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
