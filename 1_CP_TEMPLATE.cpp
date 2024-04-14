5de1379621e50b821fba2e56b55b63a70fa1f9c2
prasantdas <93581641+prasantdas@users.noreply.github.com>
2024-04-15 03:47:30 +0530

    Update 1_CP_TEMPLATE.cpp

--- 1_CP_TEMPLATE.cpp
+++ 1_CP_TEMPLATE.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*
- * Author    : Prasant Das
- * Date      : 15:06:2023
- */
+  /*
+   * Author    : Prasant Das
+   * Date      : 15:06:2023
+  */
+
+        // MACROS
 
 #define mod 1000000007
 #define INF 1e18
@@ -27,16 +29,47 @@ typedef priority_queue<int> maxheap;
 typedef priority_queue<int, vector<int>, greater<int>> minheap;
 #define for0(i,n)  for(ll i=0; i<n; i++)
 #define for1(i,n)  for(ll i=1; i<=n; i++) 
+
+       //  DEBUGGING
+
+#ifndef ONLINE_JUDGE
+#define debug(x){cerr<<#x<<" ";_print(x);cerr<<endl;}
+#else
+#define debug(x)
+#endif
+void _print(int a){cerr<<a;}
+void _print(ll a){cerr<<a;}
+void _print(char a){cerr<<a;}
+void _print(string a){cerr<<a;}
+void _print(bool a){cerr<<a;}
+void _print(double a){cerr<<a;}
+void _print(ull a){cerr<<a;}
+void _print(lld a){cerr<<a;}
+template<class T> void _print(vector<T> v1){cerr<<"[";for(T i:v1){ _print(i);cerr<<" ";}cerr<<"]";}
+template<class T> void _print(set<T> s1){cerr<<"[";for(T i:s1){ _print(i);cerr<<" ";} cerr<<"]";}
+template<class T,class V> void _print(pair<T,V> p1){cerr<<"{";_print(p1.first);cerr<<","; _print(p1.second);cerr<<"}";}
+template<class T> void _print(multiset<T> s2){cerr<<"[";for(T i:s2){ _print(i);cerr<<" ";}cerr<<"]";}
+template<class T,class V> void _print(map<T,V> m1){cerr<<"[";for(auto i:m1){ _print(i);cerr<<" ";}cerr<<"]";}
+template<class T,class V> void _print(unordered_map<T,V> m2){cerr<<"[";for(auto i:m2){_print(i);cerr<<" ";}cerr<<"]";}
+
+        // DIRECTIONS
+
 int d1[4] = { -1, 1, 0, 0};
 int d2[4] = {0, 0, -1, 1};
 int D1[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
 int D2[8] = { -1, 0, 1, -1, 1, -1, 0, 1 }; 
 
+       // SOLVE FOR EACH TEST CASES HERE
+
 void solve() {
     //Write Here
     
 }
+
 int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("error.txt","w",stderr);
+    #endif
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
